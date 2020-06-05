8de583fb34b3564055aa84bd900103614ddf4e37
Luis Felipe Menardi <luisfelipemenardi@gmail.com>
2020-06-05 16:02:44 -0300

    "finalized"

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-full stack web project by Luis F G Menardi
+full stack web project by Luis F G Menardi made during NextLevelWeek(NLW)
+promoted by Rocketseat
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados (tem que existir module.exports no arquivo)
+const db = require("./database/db.js")
+
 // configurar pasta públic
 server.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // Utilizando template engine
 const nunjucks = require("nunjucks")
@@ -21,10 +27,80 @@ server.get("/", (req, res) => {
     return res.render("index.html")
 })
 server.get("/create-point", (req, res) => {
+    // req.query: Query Strings da url
+    // console.log(req.query)
+
     return res.render("create-point.html")
 })
+
+server.post("/savepoint", (req, res) => {
+    // req.body: o corpo do nosso formulário
+    // console.log(req.body)
+
+    // inserir dados no banco de dados
+    const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (?, ?, ?, ?, ?, ?, ?);
+        `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+
+})
+
+
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    if (search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+    // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        // Mostrar a página HTML com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+
+
 })
 
 // ligar o servidor
